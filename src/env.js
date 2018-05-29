1db94d8c8599e2c4512e3bff28042152e54d9ea8
dungvan2512 <dungvan2512@gmail.com>
2018-05-29 10:41:24 +0700

    test with internet

--- src/env.js
+++ src/env.js
@@ -2,13 +2,13 @@ var env = {};
 if (process.env.SSN_ENV === 'development') {
     env = {
         url: 'http://localhost:3000',
-        api: 'http://localhost',
+        api: 'http://a9eae468.ngrok.io',
         s3Endpoint: 'http://e78f6947.ngrok.io/socker-social-network-api/'
     }
 } else {
     env = {
         url: 'http://localhost:3000',
-        api: 'http://localhost',
+        api: 'http://a9eae468.ngrok.io',
         s3Endpoint: 'http://e78f6947.ngrok.io/soccer-social-network-api/'
     }
 }
